08cd929050221c9127840bf8f9cb71550c771e44
palascinochristian <palascinochristian@gmail.com>
2025-04-29 01:13:48 +0200

    Bonus 1

--- index.php
+++ index.php
@@ -4,11 +4,11 @@ class Movie {
     public $title;
     public $director;
     public $releaseYear;
-    public $genre;
+    public $genres = [];
    
 
-    public function __construct($title, $director, $releaseYear, $genre) {
-        $this->genre = $genre;
+    public function __construct($title, $director, $releaseYear, $genres = []) {
+        $this->genres = $genres;
         $this->title = $title;
         $this->director = $director;
         $this->releaseYear = $releaseYear;
@@ -32,9 +32,9 @@ class Genre {
 }
 
 
-$movie1 = new Movie("Die Hard", "John McTiernan", 1988, new Genre("Action"));
-$movie2 = new Movie("The Matrix", "Lana Wachowski, Lilly Wachowski", 1999, new Genre("Sci-Fi"));
-$movie3 = new Movie("The Godfather", "Francis Ford Coppola", 1972, new Genre("Crime"));
+$movie1 = new Movie("Die Hard", "John McTiernan", 1988, [new Genre("Action"), new Genre("Thriller")]);
+$movie2 = new Movie("The Matrix", "Lana Wachowski, Lilly Wachowski", 1999, [new Genre("Sci-Fi"), new Genre("Action")]);
+$movie3 = new Movie("The Godfather", "Francis Ford Coppola", 1972, [new Genre("Crime"), new Genre("Drama")]);
 
 
 ?>
@@ -51,9 +51,33 @@ $movie3 = new Movie("The Godfather", "Francis Ford Coppola", 1972, new Genre("Cr
 <body>
    <h1> Movies List </h1>
     <ul>
-          <li><?php echo $movie1->getTitle(); ?> - Genre: <?php echo $movie1->genre->getType(); ?></li>
-          <li><?php echo $movie2->getTitle(); ?> - Genre: <?php echo $movie2->genre->getType(); ?></li>
-          <li><?php echo $movie3->getTitle(); ?> - Genre: <?php echo $movie3->genre->getType(); ?></li>
+        <li>
+            <h2><?php echo $movie1->getTitle(); ?></h2>
+            <p>Genres: 
+                <?php foreach ($movie1->genres as $genre) {
+                    echo $genre->getType() . ", ";
+                } ?>
+            </p>
+        </li>
+        <li>
+            <h2><?php echo $movie2->getTitle(); ?></h2>
+    
+            <p>Genres: 
+                <?php foreach ($movie2->genres as $genre) {
+                    echo $genre->getType() . ", ";
+                } ?>
+            </p>
+        </li>
+        <li>
+            <h2><?php echo $movie3->getTitle(); ?></h2>
+            <p>Genres: 
+                <?php foreach ($movie3->genres as $genre) {
+                    echo $genre->getType() . ", ";
+                } ?>
+            </p>
+        </li>
+
+        <?php  //echo var_dump($movie1, $movie2, $movie3)?>
      </ul>
 </body>
 </html>
